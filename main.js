83406cfe5c308cb46dc822de548f53489e2b6dc6
noamyr <syrganzi@gmail.com>
2023-12-11 09:32:47 +0100

    Refresh file case

--- main.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("background_wrap").style.backgroundPosition = getRndInteger(1,100).toString() + "% " + getRndInteger(1,100).toString() + "%";
-function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min) ) + min;
-  }
